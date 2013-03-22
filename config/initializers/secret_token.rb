46e1059ed139b17f877e1006dad0694281347b87
Alvaro Naveda <naveda89@gmail.com>
2013-03-22 16:23:11 +0100

    Added basic files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MultiFavicon::Application.config.secret_token = 'ccdf920d9849430c0ecb5878c5d0a670be78ed82cefa782c224aff25f05285afa5df0684faa5aa4c9fa6ea2c4c6b906b33b990312155cb412a547685c54d0388'
